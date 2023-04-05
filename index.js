63d3d98210393c3a68462696142085745404781f
BibatRalph <BibatRalph@gmail.com>
2023-04-06 01:15:45 +0800

    DashBoard UI FIX

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.get("/", (req, res) => {
-    res.send({ message: "Hello World!" });
+    res.send({ message: "App working" });
 });
 
 app.use("/api/v1/users", userRouter);
