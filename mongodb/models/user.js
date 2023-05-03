4b69e3ae33d40a5307cbad55a6ae2564a08b5f21
BibatRalph <BibatRalph@gmail.com>
2023-05-04 01:39:23 +0800

    Password Update working

--- mongodb/models/user.js
+++ mongodb/models/user.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true },
     avatar: { type: String, required: false },
     role: { type: String, required: false },
-
+    password: { type: String, required: false },
     // All created application
     allProperties: [{ type: mongoose.Schema.Types.ObjectId, ref: "Applicants" }],
     // All created jobs
